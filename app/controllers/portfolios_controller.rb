25e20bdf5177d210af0a4d2fbcf08869ab1720dc
ivanmatas <ivan.matas@locastic.com>
2018-05-20 13:02:49 +0200

    integrate new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,25 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+
+    if @portfolio_item.save
+      redirect_to portfolios_path, notice: 'Your portfolio item has been created!'
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def portfolio_item_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
